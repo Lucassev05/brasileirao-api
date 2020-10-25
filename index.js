d7574b00ce5e266f54256be2fa7a62670beca53c
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-10-25 10:45:03 -0300

    feat: adding koa cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 const router = require('./src/routes');
 
 require('dotenv').config();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8000;
 const server = new Koa();
 
 server.use(bodyParser());
+server.use(cors());
 server.use(router.routes());
 
 server.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
