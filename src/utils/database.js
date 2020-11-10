c829bbb3410d9d0b4cd6135e8a6f692b6446fe26
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-11-10 20:08:04 -0300

    fix: connection database

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,11 +2,7 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	port: process.env.DB_PORT,
-	password: process.env.DB_PW,
-	database: process.env.DB_NAME,
+	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false,
 	},
